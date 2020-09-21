74a2072b7d473df7bdcf983fb2eaed6e9851b6bc
Mr.deng <1693172461@qq.com>
2020-09-21 22:21:27 +0800

    first

---
+++ src/main/java/com/dxx/bokeadmin/BokeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dxx.bokeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokeAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxx/bokeadmin/BokeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxx.bokeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
